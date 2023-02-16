c5909600442f28aa2c2a4df07da3deb03c9afebb
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-02-16 13:23:47 +1300

    Started troubleshooting and fixing code

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -12,7 +12,6 @@ var GameBoard = function () {
 			board[i].push(Cell());
 		}
 	}
-
 	// This will be the method of getting the entire board that our UI will eventually need to render it.
 	const getBoard = () => board;
 
@@ -25,9 +24,15 @@ var GameBoard = function () {
 			board[row][column].addMarker(player);
 		}
 	}
-	// an interface for the rest of our application to interact with the board
+	const printBoard = () => {
+		const boardWithCellValues = board.map((row) =>
+			row.map((cell) => cell.getValue())
+		);
+		console.log(boardWithCellValues);
+	};
 
-	return { getBoard, placeMarker };
+	// an interface for the rest of our application to interact with the board
+	return { getBoard, placeMarker, printBoard };
 };
 
 // factory function pattern
@@ -40,18 +45,32 @@ const Cell = () => {
 	const addMarker = (player) => {
 		value = player.getMarker;
 	};
-	// How we will retrieve the current value of this cell through closure
+	// How we will retrieve the current value of the cell marker through closure
 	const getValue = () => value;
 
 	return { addMarker, getValue };
 };
 
 // an object to control the flow of the game itself
-function gameController(player1, player2) {
+function GameController() {
+	let player1;
+	let player2;
 	const players = [player1, player2];
 	const board = GameBoard();
 
 	let activePlayer = players[0];
+
+	function startGame() {
+		player1 = player(setPlayerName("player1"), "X");
+		player2 = player(setPlayerName("player2"), "O");
+		// Initial play game message
+		printNewRound();
+	}
+
+	function setPlayerName(player) {
+		var name = prompt(`${player} Please enter your username`);
+		return name;
+	}
 
 	const switchPlayerTurn = () => {
 		activePlayer = activePlayer === players[0] ? players[1] : players[0]; // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
@@ -60,12 +79,12 @@ function gameController(player1, player2) {
 
 	const printNewRound = () => {
 		board.printBoard();
-		console.log(`${getActivePlayer().name}'s turn.`);
+		console.log(`${activePlayer.getName()}'s turn.`);
 	};
 
 	const playRound = (column, row) => {
 		console.log(
-			`Dropping ${getActivePlayer().name}'s token into cell ${row}${column}`
+			`Dropping ${activePlayer.getName()}'s token into cell ${row}${column}`
 		);
 		board.placeMarker(row, column, activePlayer);
 		//This is where we would check for a winner and handle that logic
@@ -78,20 +97,57 @@ function gameController(player1, player2) {
 		printNewRound();
 	};
 
-	// Initial play game message
-	printNewRound();
+	const checkWinner = () => {
+		const boardState = board.getBoard;
+		checkRows();
 
-	return { getActivePlayer, playRound, getBoard: board.getBoard };
+		//check if diagonal winner from top left and right cells
+
+		//Check if horizontal winner along all 3 rows
+		function checkRows() {
+			const checker = [];
+			for (var i = 0; i < boardState.length || checker === 3; i++) {
+				const rowValue = boardState[i][0];
+				checker = [];
+				for (var j = 0; j < boardState.length; j++) {
+					if (rowValue === boardState[i][j]) {
+						checker.push.boardState[i][j];
+					} else if (checker.length === 3) {
+						checker[0].getValue
+					} else {
+						continue;
+					}
+				}
+			}
+		}
+
+		//Check if vertical winner along all 3 columns
+
+		//else: if none of the above conditions are met & all cells are filled then it is a draw
+	};
+
+	return {
+		getActivePlayer,
+		playRound,
+		getBoard: board.getBoard,
+		checkWinner,
+		startGame,
+	};
 }
 
 // module will leverage an updateScreen pattern https://studio.code.org/docs/concepts/patterns/update-screen-pattern/
-// module pattern
+// module pattern as we only need one of these 
 //The function that will render the contents of the gameboard array to the webpage
 // take some data about our game, such as the state of the game board and which player's turn it is, and update the screen each time a player takes their turn.
-var DisplayController = function () {
-	const game = GameController();
+const DisplayController = function () {
+	var game = GameController();
+	const startButton = document.querySelector(".start-restart");
+	startButton.addEventListener("click", () => {
+		game.startGame;
+	});
+
 	const playerTurnDiv = document.querySelector(".turn");
-	const boardDiv = document.querySelector(".board");
+	const boardDiv = document.querySelector(".gameBoard");
 
 	const updateScreen = () => {
 		// clear the board
@@ -102,7 +158,9 @@ var DisplayController = function () {
 		const activePlayer = game.getActivePlayer();
 
 		// Display player's turn
-		playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+		if (activePlayer !== undefined) {
+			playerTurnDiv.textContent = `${activePlayer.getName()}'s turn...`;
+		}
 
 		// Render board squares
 		board.forEach((row) => {
@@ -143,4 +201,4 @@ const player = (name, marker) => {
 	return { getName, getMarker };
 };
 
-ScreenController();
+DisplayController();
